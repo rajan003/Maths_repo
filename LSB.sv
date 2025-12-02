647e2c24cd444b082e34737ca10aa2e9dd086e82
Anurag Singh <anuragsi003@gmail.com>
2025-12-02 17:17:01 +0100

    Add LMB module for calculating leftmost set bit
    Implement Leftmost Set Bit calculation using right shift.

---
+++ LSB.sv
@@ -0,0 +1,32 @@
+///Calculating Leftmost Set bit//////////////////
+Approach-1: Right Shift the Number till its greater than 0 (>0) . 
+module LMB (
+            input logic clk;
+  input logic [NUM_PORTS-1:0] in,
+  output logic pos );
+
+
+            logic [NUM_PORTS-1:0] pos_r;
+            always@(posedge clk, posedge rst)
+                 begin 
+                             if(rst) begin 
+                                         x <= '0;
+                                         pos <= '0;
+                                         end
+                             else if(en) x <= in;
+                             else while(x>0)
+                                         begin 
+                                                x<= x >>1;
+                                                pos=pos+1;
+                                         end 
+                 end 
+
+always_comb
+            if(x==0) pos = pos_r;
+
+endmodule 
+
+
+///Approach-1
+
+/// assign pos= x & ±(x-1)
